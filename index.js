ef40b8d60c8b7da8c90688115e07f104dd5fd26b
milityan <89461325+milityan@users.noreply.github.com>
2021-10-13 23:31:32 +0900

    脆弱性をなくす
    マージ不要
    脆弱性(ぜいじゃくせい)だと悪用されやすいため

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
-const http = require('http');
-const cp = require('child_process');
+const http = require('http'); // httpというモジュール(一定の能力を持ったもの)
 const server = http.createServer((req, res) => {
   const path = req.url;
   res.writeHead(200, {
     'Content-Type': 'text/plain; charset=utf-8'
   });
-  res.end(cp.execSync('echo ' + path));
-});
+  res.end(path);
+}); // const server からここまでhttpサーバーを作成している
 const port = 8000;
 server.listen(port, () => {
   console.info('Listening on ' + port);
